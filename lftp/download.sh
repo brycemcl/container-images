07dc83a9232af4b682b0eb29ccfc2218e5b5f60c
Bryce McLachlan <brycemclachlan@172-16-222-50.home.bmclachlan.xyz>
2020-07-31 19:33:10 -0700

    .

--- lftp/download.sh
+++ lftp/download.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-HOST=$1
-LOCAL_DIR=$3
-REMOTE_DIR=$2
+LFTP_HOME=/root/
 
-lftp $HOST <<EOF
+HOST=127.0.0.1
+REMOTE_DIR=~
+LOCAL_DIR=~
+
+HOST=$1
+REMOTE_DIR=$2
+LOCAL_DIR=$3
+
+
+lftp <<EOF
+set cmd:default-protocol sftp
 set sftp:auto-confirm yes
+set cmd:interactive no
+open $HOST
 cd $REMOTE_DIR
 mirror -c -P2 --use-pget-n=5 ./ $LOCAL_DIR
 exit
