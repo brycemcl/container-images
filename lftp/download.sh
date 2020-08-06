52b9f7e5108f1404cd5dad1e53bb3e2a381277ac
Bryce McLachlan <brycemclachlan@172-16-222-50.home.bmclachlan.xyz>
2020-08-05 21:52:43 -0700

    .

--- lftp/download.sh
+++ lftp/download.sh
@@ -14,15 +14,19 @@ cp /netrc/.netrc /root/.netrc
 
 lftp <<EOF
 set cmd:default-protocol sftp
+set net:timeout 5
+set net:max-retries 30
+set net:reconnect-interval-multiplier 1
+set net:reconnect-interval-base 5
 set sftp:auto-confirm yes
 set xfer:use-temp-file yes
-set xfer:timeout 60
 set cmd:interactive no
-set pget:min-chunk-size 50m
 open $HOST
 cd $REMOTE_DIR
 mirror -v -e -c --parallel=4 ./ $LOCAL_DIR
 exit
 EOF
 
+
+#set pget:min-chunk-size 50m
 #mirror -v -e --parallel=2 --use-pget-n=5 ./ $LOCAL_DIR
